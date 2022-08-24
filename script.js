d8672693ad1493a8a2822fbc75585e9a5b3fb084
Paula-Petra <paulapetrai@gmail.com>
2022-08-24 11:10:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ $(document).ready(function(){
     });
 });
 
-// function toggle(){
-//     var blur = document.getElementById('blur');
-//     blur.classList.toggle('active')
-// }
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function closePopup() {
+    var close = document.getElementById('popup');
+    close.classList.toggle('active')
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+}
+
+var date = new Date();
+var tdate = date.getDate();
+var month = date.getMonth() + 1;
+
+if(tdate < 10){
+    date = '0' + tdate;
+}
+
+if(month < 10){
+    month = '0' + month;
+}
+
+var year = date.getUTCFullYear();
+var minDate = year + '-' + month + '-' + tdate;
+document.getElementById('date').setAttribute('min', minDate);
+document.getElementById('date2').setAttribute('min', minDate);
+
+console.log(minDate);
